6f5d54b2c5a02e2300cbc6a082c5966d50650ef7
T Mostert <theresa@isogramconsulting.com>
2022-03-11 14:03:46 +0100

    Updated filter component

--- react-next-appollo-graphql-main/components/TaskFilter.tsx
+++ react-next-appollo-graphql-main/components/TaskFilter.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Link from 'next/link';
 
 const TaskFilter: React.FC = () => {
-    return <></>
+    return (
+        <ul className="task-filter">
+                <li>
+                    <Link href="/" scroll={false}>
+                        <a>All</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/[status]" as="/active" scroll={false}>
+                        <a>Active</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/[status]" as="/completed" scroll={false}>
+                        <a>Completed</a>
+                    </Link>
+                </li>
+        </ul>
+    );
 }
 
 export default TaskFilter;
